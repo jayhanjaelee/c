f2d1f24383eeb80e697b4f7bb7677b29e5c2667a
Hanjae Lee <jayhanjaelee@gmail.com>
2026-01-27 22:02:57 +0900

    WIP: client and server.

---
+++ socket/tcp_server.c
@@ -0,0 +1,42 @@
+/**
+ * @file main.c
+ * @author Hanjae Lee (mail@hanjaelee.com)
+ * @date 2026-01-26 20:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <unistd.h>
+
+int main() {
+    char server_message[256] = "You have reached the server!";
+
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM,  0);
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    listen(server_socket, 5);
+
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL);
+    //      accept(int socket, struct sockaddr *restrict address, socklen_t *restrict address_len);
+    send(client_socket, server_message, sizeof(server_message), 0);
+
+    close(server_socket);
+
+    return 0;
+}
+
