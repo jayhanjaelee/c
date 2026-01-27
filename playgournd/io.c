9f28587196d2e01b9bfbc7c1eb3260a867b92837
Hanjae Lee <jayhanjaelee@gmail.com>
2026-01-27 21:50:38 +0900

    Add get a line then writing it into a file from standard input.

--- playgournd/io.c
+++ playgournd/io.c
@@ -7,12 +7,33 @@
 
 #include <stdio.h>  // stdio: standard input/output
 
-int main(void) {
+#include <string.h>
+
+int get_a_line_from_stdin() {
+    char *filename = "out.txt";
+
+    // read line.
+    char buf[1024];
+    FILE *fp;
+    fp = fopen(filename, "a");
+    while (fgets(buf, sizeof buf, stdin)) {
+        fwrite(buf, sizeof(char), strlen(buf), fp);
+        fflush(fp);  // ← 버퍼를 즉시 디스크에 플러시
+    }
+    fclose(fp);
+
+    return 0;
+}
+
+void get_a_char() {
     char line[256];  // char: character, 한 줄 저장 버퍼
 
     while (fgets(line, sizeof(line), stdin) != NULL) {
         fputs(line, stdout);
-    }
 
+    }
+}
+
+int main(void) {
     return 0;
 }
